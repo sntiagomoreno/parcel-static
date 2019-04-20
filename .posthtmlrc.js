99653083bf2e3d39b8af946b9d5677db77baf21e
Santiago Moreno <santiago@Santiagos-MacBook-Pro.local>
2019-04-19 23:13:31 -0500

    Initial commit

---
+++ .posthtmlrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: {
+        "posthtml-include": {
+            root: __dirname + '/src'
+        },
+        "posthtml-modules": {
+            root: __dirname + '/src'
+        }
+    }
+};
